49996560871daf27472cce60b87bc6e274ca55f3
Roman <roman.batiuk87@gmail.com>
2022-03-09 18:49:00 +0200

    connection to postgres with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@app/app.controller';
 import { AppService } from '@app/app.service';
 import { TagModule } from '@app/tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getPstgresOrmConfig } from '@app/configs/typeorm.config';
 
 @Module({
-	imports: [TagModule],
+	imports: [
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getPstgresOrmConfig,
+		}),
+		TagModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
